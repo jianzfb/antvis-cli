7540c6a3d265ba46fe6de2b1af5098b3aeb6ec7d
jian.fbehind <jian.fbehind@gmail.com>
2024-08-26 01:24:47 +0000

    debug exit abnormal

--- antvis/client/__init__.py
+++ antvis/client/__init__.py
@@ -16,37 +16,34 @@ __exit_flag = False
 
 
 def handle_exit():
-  global __exit_flag
-  if not __exit_flag:
-    # project/experiment
-    if mlogger.getEnv() is None or mlogger.getEnv().dashboard is None:
-      return
-
-    #
-    project = mlogger.getEnv().dashboard.project
-    experiment = mlogger.getEnv().dashboard.experiment_name
-
-    if project is not None and experiment is not None:
-      # 更新未上传数据
-      mlogger.update()
-
-      # 日志退出
-      logging.info('Prepare exit {}/{} experiment logger'.format(project, experiment))
-      mlogger.exit()
-
-      # print log
-      logging.info('Finish {}/{} experiment logger'.format(project, experiment))
-
-
-def handle_exception(exc_type, exc_value, exc_traceback):
-  global __exit_flag
-  __exit_flag = True
-  # print error info
-  traceback.print_exception(exc_type, exc_value, exc_traceback)
-
+  # 是否完成初始化
   if mlogger.getEnv() is None or mlogger.getEnv().dashboard is None:
     return
 
+  # 项目名称/实验名称
+  project = mlogger.getEnv().dashboard.project
+  experiment = mlogger.getEnv().dashboard.experiment_name
+
+  if project is not None and experiment is not None:
+    # 更新未上传数据
+    mlogger.update()
+
+    # 日志退出
+    logging.info('Prepare exit {}/{} experiment logger'.format(project, experiment))
+    mlogger.exit()
+
+    # print log
+    logging.info('Finish {}/{} experiment logger'.format(project, experiment))
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+  # print error info
+  traceback.print_exception(exc_type, exc_value, exc_traceback)
+  # 是否完成初始化
+  if mlogger.getEnv() is None or mlogger.getEnv().dashboard is None:
+    return
+
+  # 项目名称/实验名称
   project = mlogger.getEnv().dashboard.project
   experiment = mlogger.getEnv().dashboard.experiment_name
 
